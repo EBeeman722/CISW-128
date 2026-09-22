c0bb139914acd62e6557892f50c2630fc34e05bb
Ebeeman722 <91504639+EBeeman722@users.noreply.github.com>
2026-09-22 17:20:25 -0600

    Add files via upload

---
+++ Week 5/homework/scripts.js
@@ -0,0 +1,23 @@
+document.getElementsByName('body').innerHTML += "<p>Result: " + total + "</p>";
+
+const isWebPage = true;
+let mathPartOne = 78;
+let mathPartTwo = 43;
+const spider = 'Jerimiah';
+let dog = 'chein';
+
+let total = mathPartOne + mathPartTwo;
+let squared = mathPartOne ^ 2;
+
+document.body.innerHTML += "<p>Result: " + total + "</p>";
+
+console.log(isWebPage);
+console.log(mathPartOne);
+console.log(mathPartTwo);
+console.log(spider);
+console.log(dog);
+console.log(total);
+console.log(squared);
+console.log('The dogs name is ' + dog);
+console.log('the other '+dog+' is named '+spider);
+document.body.innerHTML += "<p>Result: " + total + "</p>";
